fd6d5e1dd415c04fec000e8fddd294fa48493e45
Tkoonce1 <112811613+Tkoonce1@users.noreply.github.com>
2022-11-08 15:07:33 -0500

    first coment

--- js-practice.js
+++ js-practice.js
@@ -3,46 +3,65 @@
 //1. data types built-in methods, variables 
 
 //a. define a variable and give it a string value
-
+let cat="get food";
 //b. define a variable and give it a number value
-
+let number= 1;
 //c. define a variable and give it a boolean value
-
+let boolean= true;
 //2. if else, ternary 
 //a. define a variable 'metrocard' and give it a value of 5
-
+let metrocard=2; 
 //b. write an if statement that has a condition to check if the value of metro card is greater than 2.75, and display the message "you have enough to ride the train", if the condition is true
-
+if(metrocard>=2.75) {
+    console.log("you have enough to ride the train");
+}
 //c.write an else statement that displays the message "you do not have enough to pay fare sorry"
-
+else{
+    console.log("you do not have enough to pay fare sorry")
+}
 //d. write the above if/else statement again as a ternary 
-
+metrocard>=2.75?console.log("you have enough to ride the train"):console.log("you do not have enough to pay fare sorry")
 //3. conditionals 2 (1 pt)
 //a.  complete this codecademy project: https://www.codecademy.com/courses/introduction-to-javascript/projects/magic-eight-ball-1 
 //add a screenshot showing your completion of this project to the root folder of this repo
 
 //4. functions 
 //a. write a function named helloWorld that returns the value 'Hello World!' 
+function helloWorld(){
 
+    return "hello world"
+}
 //b. console log the value returned from running the function helloWorld
-
+console.log(helloWorld())
 
 //3. arrays
 //a. define a variable myArray and assign it to a value of an array. Give the array at least 5 elements
-
+let myArray=[4, 2, "get food",true, 7 ]
 //b. an array's index starts at:
-//
+//0
+for(let index=0;index<=4;index=index+1){
+    console.log( myArray[index])
+}
 
 
 //4. loops 
 //a. write a for loop that console logs even numbers starting at 10 and decrementing to 0 
-
+for(let i=10;i>=0;i=i-1){
+    if (i%2==0){
+        console.log(i)
+    }
+     
+}
 
 //5. objects
 //a. define a variable named 'bike' 
-
+let bike ;
 //b. assign an object to bike 
-
+bike={
+    handlebars:"round", 
+    color:"red",
+    wheels:"rubber"
+}
 //c. give the object 3 properties: handlebars, color, wheels 
 
 //d. give each property a value 
